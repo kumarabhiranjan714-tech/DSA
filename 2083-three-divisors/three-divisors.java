7726655478d47f1d97efc5487efd7d24030f1208
kumarabhiranjan714-tech <kumarabhiranjan714@gmail.com>
2026-07-13 23:13:57 +0530

    Time: 1 ms (78.67%) | Memory: 42.4 MB (5.98%) - LeetSync

--- 2083-three-divisors/three-divisors.java
+++ 2083-three-divisors/three-divisors.java
@@ -4,13 +4,10 @@ class Solution {
 
         for(int i=1;i<=n;i++){
             if(n%i==0){
-                divisor_count = divisor_count+1;
+                divisor_count +=1;
             }
         }
         
-        if(divisor_count==3){
-            return true;
-        }
-        return false;
+        return divisor_count==3 ; 
     }
 }
